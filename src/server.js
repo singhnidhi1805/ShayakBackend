9948d138c0142b252008e97dc59eae021786e552
Nidhi kumari <115169748+singhnidhi1805@users.noreply.github.com>
2025-04-26 22:29:27 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -50,7 +50,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`);
   logger.info('Environment:', process.env.NODE_ENV);
